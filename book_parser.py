50850b14df123644705be8134eaa37ccbdd06712
Aleksandr Skripko <skripkoae@gmail.com>
2024-07-11 14:57:58 +0300

    download books covers

--- book_parser.py
+++ book_parser.py
@@ -1,6 +1,9 @@
+import os
+
 import requests
 from pathlib import Path
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin, urlsplit
 
 
 def book_request(book_id):
@@ -15,21 +18,22 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
+def serialize_book(book_response, book_id, base_url):
+    soup = BeautifulSoup(book_response.text, 'lxml')
+    book = {'id': book_id}
+    book_header = soup.find(id='content').find('h1').text.split('\xa0')
+    book['title'] = book_header[0]
+    book['author'] = book_header[2]
+    book['img'] = urljoin(base_url, soup.find(id='content').find('img')['src'])
+    return book
+
+
 def book_download_request(book_id: int):
     url = 'https://tululu.org/txt.php'
     payload = {'id': book_id}
     book_download_response = requests.get(url, params=payload, allow_redirects=False)
     book_download_response.raise_for_status()
     return book_download_response
-
-
-def serialize_book(book_response, book_id):
-    soup = BeautifulSoup(book_response.text, 'lxml')
-    book = {'id': book_id}
-    book_header = soup.find(id='content').find('h1').text.split('\xa0')
-    book['title'] = book_header[0]
-    book['author'] = book_header[2]
-    return book
 
 
 def download_book(dir_name, book_title, response, book_id):
@@ -39,9 +43,21 @@ def download_book(dir_name, book_title, response, book_id):
     return filename
 
 
+def download_book_cover(book_img_url_path, dir_name, book_image_full_url):
+    response = requests.get(book_image_full_url)
+    response.raise_for_status()
+    filename = os.path.join(dir_name, book_img_url_path.split('/')[2])
+    with open(filename, 'wb') as file:
+        file.write(response.content)
+    return filename
+
+
 def main():
-    dir_name = Path('books')
-    dir_name.mkdir(exist_ok=True)
+    base_url = 'https://tululu.org'
+    books_dir_name = Path('books')
+    books_dir_name.mkdir(exist_ok=True)
+    images_dir_name = Path('images')
+    images_dir_name.mkdir(exist_ok=True)
     first_book_id = 1
     last_book_id = 10
     for book_id in range(first_book_id, last_book_id + 1):
@@ -50,9 +66,10 @@ def main():
             check_for_redirect(book_response)
         except requests.HTTPError:
             continue
-        book = serialize_book(book_response, book_id)
-        download_book(dir_name, book['title'], book_download_request(book_id), book['id'])
-        print(book_id)
+        book = serialize_book(book_response, book_id, base_url)
+        download_book(books_dir_name, book['title'], book_download_request(book_id), book['id'])
+        download_book_cover(urlsplit(book['img'])[2], images_dir_name, book['img'])
+        print(book)
 
 
 main()
